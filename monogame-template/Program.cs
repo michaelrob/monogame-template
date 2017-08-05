eedca5b16d71d227cf8970c82aa6977b9bd260f7
Michael Robinson <seraph890@gmail.com>
2017-08-05 22:22:32 +1000

    updated the csproj

---
+++ monogame-template/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonoGameTemplate {
+	static class Program {
+		private static Game1 game;
+
+		[STAThread]
+		static void Main() {
+			game = new Game1();
+			game.Run();
+		}
+	}
+}
